a928510607718391867393ea9393d50e71af00e6
yzx_glob <yzx_globqq.com>
2023-03-08 19:40:21 +0800

    info

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import elemeui from 'element-ui'
+import vueRoute from 'vue-router'
+import index from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(elemeui)
+Vue.use(vueRoute)
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
+  router:index
 }).$mount('#app')
